4312c6980b023b53abb6458a18c4639900d480a6
Esperanza Cardenas <esperanzacardenas@Esperanzas-MBP.home>
2021-02-04 16:35:20 -0800

    added default image

--- src/Components/Add/Add.js
+++ src/Components/Add/Add.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import config from '../../config';
 import Context from '../../Context';
 import TokenService from '../../services/token-service';
-
+import reflectImg from '../../reflectImg.jpg'
 export default class Add extends Component{
     static contextType = Context;
     state = {
