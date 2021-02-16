90f46332374c6f546b16c4aca016de985326bc4f
Esperanza Cardenas <esperanzacardenas@Esperanzas-MBP.home>
2021-02-15 16:40:54 -0800

    editing to include heroku url.

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 export default {
     PORT: process.env.PORT || 8000,
     API_BASE_URL:
-        process.env.API_BASE_URL || 'http://localhost:8000/api',
+        process.env.API_BASE_URL || `https://reflect-server.herokuapp.com/api`,
     TOKEN_KEY: process.env.TOKEN_KEY || 'reflect-token',
 };
--- src/services/auth-api-service.js
+++ src/services/auth-api-service.js
@@ -1,7 +1,6 @@
-/* eslint-disable import/no-anonymous-default-export */
 import config from '../config';
 
-export default {
+const AuthAPIService = {
     async postUser(user){
         const res = await fetch(`${config.API_BASE_URL}/users`, {
             method: 'POST',
@@ -18,3 +17,5 @@ export default {
         }).then((res) => !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json())
     },
 }
+
+export default AuthAPIService;
