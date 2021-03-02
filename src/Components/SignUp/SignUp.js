9cc67e6be3cd3d9837ab2495201d30e8292e57b6
Esperanza Cardenas <esperanzacardenas@Esperanzas-MBP.home>
2021-03-02 15:16:11 -0800

    fixing forms

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,54 +1,57 @@
-import React, { Component } from 'react';
-import AuthAPIService from '../../services/auth-api-service';
-import './SignUp.css';
+import React, { Component } from "react";
+import AuthAPIService from "../../services/auth-api-service";
+import "./SignUp.css";
 
-export default class SignUp extends Component{
-    state={
-        error: null,
-    };
+export default class SignUp extends Component {
+	state = {
+		error: null,
+	};
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        const { email, password, confirmPassword } = e.target;
-        this.setState({ error: null});
+	handleSubmit = (e) => {
+		e.preventDefault();
+		const { email, password, confirmPassword } = e.target;
+		this.setState({ error: null });
 
-        if(password.value !== confirmPassword.value){
-            return this.setState({ error: 'Passwords do not match' });
-        }
-        AuthAPIService.postUser({
-            email: email.value,
-            password: password.value,
-        })
-        .then((user) => {
-            this.props.history.push('/login');
-        })
-        .catch((res) => {
-            this.setState({ error: res.error })
-        })
-    };
+		if (password.value !== confirmPassword.value) {
+			return this.setState({ error: "Passwords do not match" });
+		}
+		AuthAPIService.postUser({
+			email: email.value,
+			password: password.value,
+		})
+			.then((user) => {
+				this.props.history.push("/login");
+			})
+			.catch((res) => {
+				this.setState({ error: res.error });
+			});
+	};
 
-    render(){
-        return(
-            <form className='form' onSubmit={this.handleSubmit}>
-                {this.state.error && <p className='error'>{this.state.error}</p>}
-                <h1>Sign Up</h1>
-                <label htmlFor='email'>Email: </label>
-                <br></br>
-                <input htmlFor='email' type='email' name='email' required/>
-                <br></br>
-                <label htmlFor='password'>Password: </label>
-                <br></br>
-                <input htmlFor='password' type='password' name='password' required/>
-                <br></br>
-                <label htmlFor='confirmPassword'>Confirm Password:</label>
-                <br></br>
-                <input htmlFor='confirm-password' type='password' name='confirmPassword' required/>
-                <br></br>
-                <br></br>
-                <button type='submit'>
-                    Sign Up
-                </button>
-            </form>
-        )
-    }
+	render() {
+		return (
+			<form className="form" onSubmit={this.handleSubmit}>
+				{this.state.error && <p className="error">{this.state.error}</p>}
+				<h1>Sign Up</h1>
+				<label htmlFor="email">Email: </label>
+				<br></br>
+				<input htmlFor="email" type="email" name="email" required />
+				<br></br>
+				<label htmlFor="password">Password: </label>
+				<br></br>
+				<input htmlFor="password" type="password" name="password" required />
+				<br></br>
+				<label htmlFor="confirmPassword">Confirm Password:</label>
+				<br></br>
+				<input
+					htmlFor="confirm-password"
+					type="password"
+					name="confirmPassword"
+					required
+				/>
+				<br></br>
+				<br></br>
+				<button type="submit">Sign Up</button>
+			</form>
+		);
+	}
 }
